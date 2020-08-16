3966cf68955152d81852c8d25de2395c24574bac
Shahadat <shahadat@finder-lbs.com>
2020-08-16 11:53:16 +0600

    Updates

---
+++ precache-manifest.a80e01ccd09ee310d9348b90433e6bd5.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf3bd1fb7eed0e3181fee7ec45b2c7d1",
+    "url": "/genex-digital-assignment/index.html"
+  },
+  {
+    "revision": "fe3dfc3df9dc83fe7142",
+    "url": "/genex-digital-assignment/static/css/main.229fe38c.chunk.css"
+  },
+  {
+    "revision": "65111ff620409ea0f580",
+    "url": "/genex-digital-assignment/static/js/2.90ab7614.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/genex-digital-assignment/static/js/2.90ab7614.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe3dfc3df9dc83fe7142",
+    "url": "/genex-digital-assignment/static/js/main.c7e45591.chunk.js"
+  },
+  {
+    "revision": "52b6140b1758fafbb23d",
+    "url": "/genex-digital-assignment/static/js/runtime-main.9d9284c7.js"
+  },
+  {
+    "revision": "e114141db8944975a6d4fc630b7c65b7",
+    "url": "/genex-digital-assignment/static/media/close.e114141d.svg"
+  },
+  {
+    "revision": "867261b695bbfafe447666185f6e7c0b",
+    "url": "/genex-digital-assignment/static/media/full-screen.867261b6.svg"
+  },
+  {
+    "revision": "7a0e54ad07f1d1263239e3280d28e36a",
+    "url": "/genex-digital-assignment/static/media/video.7a0e54ad.svg"
+  }
+]);
